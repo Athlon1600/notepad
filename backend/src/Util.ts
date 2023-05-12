4a663383d28f5b7f2a7ecba4c7eacf1713af0707
Athlon1600 <Athlon2400@gmail.com>
2023-05-12 12:03:50 -0500

    Update Util.ts

--- backend/src/Util.ts
+++ backend/src/Util.ts
@@ -13,3 +13,24 @@ export const uidFromAuthKey = function (data: string) {
 
     return base64url.encode(hash).substr(0, maxLen);
 }
+
+// inclusive, exclusive
+export const randomInteger = (min?: number, max?: number): number => {
+
+    if (typeof min == 'undefined') {
+        min = 0;
+    }
+
+    if (typeof max === 'undefined') {
+        max = Number.MAX_SAFE_INTEGER;
+    }
+
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
+
+// must contain only: contain 0-9a-f
+export const isHexString = (str: string): boolean => {
+    return str.replace(/[a-f0-9]/ig, '').length === 0;
+}
